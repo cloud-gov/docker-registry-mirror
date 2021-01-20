a48c01d29adb4045555e1ee8c50dc0da3529b1b3
Steve Greenberg <steve.greenberg@gsa.gov>
2021-01-20 09:59:32 -0700

    start docker process then pull

--- ci/tasks/test-pull-succeeds.sh
+++ ci/tasks/test-pull-succeeds.sh
@@ -2,9 +2,22 @@
 
 set -ex
 
-docker --version
+docker network create pull-network
 
-docker pull ${MIRROR_HOSTNAME}.app.cloud.gov:443/18fgsa/concourse-task
+docker run --name pull-docker \
+  -e DOCKER_TLS_CERTDIR=/certs \
+  --network pull-network \
+  --network-alias docker \
+  -v some-docker-certs-ca:/certs/ca \
+  -v some-docker-certs-client:/certs/client \
+  --privileged \
+  docker:dind &> /dev/null &
+
+docker run -it --rm \
+  -e DOCKER_TLS_CERTDIR=/certs \
+  --network pull-network \
+  -v some-docker-certs-client:/certs/client:ro \
+  docker:latest pull ${MIRROR_HOSTNAME}.app.cloud.gov:443/hello-world
 
 if [ $? != 0 ]; then
   echo "Docker pull through registry failed"
