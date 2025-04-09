d0261ed0b4f592dda3ca329f9a3b94585a74ebff
Robert Gottlieb <robert.gottlieb@gsa.gov>
2025-04-09 18:12:51 -0400

    updated two scripts from v2 to v3

--- ci/tasks/deploy-registry.sh
+++ ci/tasks/deploy-registry.sh
@@ -13,11 +13,11 @@ space_guid=$(cf space --guid $CF_SPACE)
 svc_instance_name="docker-registry-mirror-s3"
 svc_key_name="registry-key"
 svc_key_guid=$(cf service-key --guid $svc_instance_name $svc_key_name)
-svc_key_json=$(cf curl "/v2/service_keys/${svc_key_guid}")
-s3_access_key=$(echo "$svc_key_json" | jq -r '.entity.credentials.access_key_id')
-s3_bucket=$(echo "$svc_key_json" | jq -r '.entity.credentials.bucket')
-s3_region=$(echo "$svc_key_json" | jq -r '.entity.credentials.region')
-s3_secret_key=$(echo "$svc_key_json" | jq -r '.entity.credentials.secret_access_key')
+svc_key_json=$(cf curl "/v3/service_credential_bindings/${svc_key_guid}/details")
+s3_access_key=$(echo "$svc_key_json" | jq -r '.credentials.access_key_id')
+s3_bucket=$(echo "$svc_key_json" | jq -r '.credentials.bucket')
+s3_region=$(echo "$svc_key_json" | jq -r '.credentials.region')
+s3_secret_key=$(echo "$svc_key_json" | jq -r '.credentials.secret_access_key')
 
 cf push -f source/registry/manifest.yml \
   --strategy rolling \
